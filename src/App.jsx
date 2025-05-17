375d6fe2e38ae2fd3987393375b54f643071aa00
Ernest da Cruz <ernest.da.cruz@deemaze.com>
2025-05-17 11:10:10 +0100

    More wins

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router';
 import Sidenav from './Sidenav';
 import Dashboard from './Dashboard';
 
-import './App.css'
+import './App.scss';
 
 function Calendar() {
   return (
