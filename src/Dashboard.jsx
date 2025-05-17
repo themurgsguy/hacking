dceb0fd439b378e736fcd43f620d3bcb0d5a25b0
Ernest da Cruz <ernest.da.cruz@deemaze.com>
2025-05-17 10:57:40 +0100

    Minor Wins

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,4 +1,14 @@
-import './dashboard.css';
+import classNames from 'classnames';
+
+function Avatar({ className, name }) {
+  const initials = name.match(/(\b\S)/g).slice(0, 2).join('');
+
+  return (
+    <div className={classNames('avatar', className)}>
+      {initials}
+    </div>
+  );
+};
 
 function Dashboard() {
   const today = new Date();
@@ -10,10 +20,18 @@ function Dashboard() {
 
   return (
     <div className="container dashboard">
-      <h1 className="welcome">Welcome back, Conway</h1>
-      <h2 className="date">
-        {today.toLocaleDateString(undefined, dateOptions)}
-      </h2>
+      <header className="header">
+        <Avatar name="Conway Mustagagaza"/>
+        <div>
+          <h1 className="welcome">Welcome back, Conway</h1>
+          <h2 className="date">
+            {today.toLocaleDateString(undefined, dateOptions)}
+          </h2>
+        </div>
+        <button className="button">
+          <span className="material-symbols-rounded">add</span>
+        </button>
+      </header>
     </div>
   );
 }
